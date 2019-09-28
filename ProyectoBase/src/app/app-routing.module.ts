c936bc7dd363bc34809377466a46af80b3098811
dDiego-g <diego_a981@live.com.mx>
2019-09-28 09:02:08 -0500

    Practica login

--- ProyectoBase/src/app/app-routing.module.ts
+++ ProyectoBase/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RegistrarPage } from './auth/registrar/registrar.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./auth/login/login.module').then(m => m.LoginPageModule)
   },
-  { path: 'registrar', loadChildren: './auth/registrar/registrar.module#RegistrarPageModule' },
+  { path: 'registro', component: RegistrarPage },
+  { path: 'registrar', component: RegistrarPage },
   { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' }
 
 ];
--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      confirmacionContrasena: ['', Validators.required]
+    });
+   }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarPassword: ['', Validators.required]
+    }, {
+      validator: this.MustMatch('password', 'confirmarPassword')
+    });
   }
 
+  get f() {return this.registerForm.controls;}
+
+  registrar() {
+    console.log(this.registerForm.valid);
+  }
+
+  MustMatch(controlName: string, matchingControlName: string){
+    return true;
+  }
 }
